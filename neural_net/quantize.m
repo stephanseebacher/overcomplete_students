9e92fde5ebe7344cde38c2a58731f8f7b42e9418
David <chdavid@student.ethz.ch>
2015-06-03 08:46:59 +0200

    Optimized version but bug in reconstruction

--- neural_net/quantize.m
+++ neural_net/quantize.m
@@ -1,20 +1,25 @@
 function [ quantized_data ] = quantize(x,bit_number)
 %quantize input data using bit_number bits
 
-bit_array=[];
+bit_array=char(zeros(length(x)*bit_number,1));
+j=1;
 for i=1:length(x)
-    bit_array=[bit_array get_bit_encoding(x(i),bit_number)];
+    bit_array(j:(j+bit_number-1))=get_bit_encoding(x(i),bit_number);
+    j=j+bit_number;
 end
 
 %now stores bit array in uint8 var
-quantized_data=[];
+
+quantized_data=uint8(zeros(1,ceil(length(x)*bit_number/8))); %8 because unit8 used!
 i=1;
-while ( i<=length(bit_array))
+%counter for uint8 vars used
+t=1;
+while ( i<length(bit_array))
     %take new int8 number
     cur_int=int8(0);
     for j=1:8
         %if 1 set bit
-        if(str2num(bit_array(i)))
+        if(str2double(bit_array(i)))
             tmp=bitset(cur_int,j);
             cur_int=int8(tmp);
         end
@@ -23,7 +28,8 @@ while ( i<=length(bit_array))
             break
         end
     end
-    quantized_data=[quantized_data cur_int];
+    quantized_data(t)=cur_int;
+    t=t+1;
 end
 
 end
